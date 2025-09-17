b8d6581c8eb3bf940d020cba6242f744c6a7cbf5
BKarret24 <dipirogov@edu.hse.ru>
2025-09-17 02:35:21 -0700

    9-grade work

--- dipirogovPW1/ViewController.swift
+++ dipirogovPW1/ViewController.swift
@@ -8,23 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var button: UIButton!
     @IBOutlet var views: [UIView]!
+    
+    // MARK: - Constants
+    private let animeDur: TimeInterval = 3.14
+    private let minCornerRad: Double = 0
+    private let maxCornerRad: Double = 30
+    private let minColorComp: Double = 0
+    private let maxColorComp: Double = 1
+    private let colorAlpha: Double = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    // MARK: - button Animations
     @IBAction func buttonWasPressed(_ sender: Any) {
-        for view in views{
-            view.backgroundColor = UIColor(
-                displayP3Red: .random(in: 0...1),
-                green: .random(in: 0...1),
-                blue: .random(in: 0...1),
-                alpha: 1
-            )
+        button.isEnabled = false
+        var colors = getUniqueColors()
+        var corners = getUniqueCorners()
+        
+        
+        UIView.animate(withDuration: animeDur, animations: {
+            for view in self.views{
+                view.backgroundColor = colors.removeFirst()
+                view.layer.cornerRadius = corners.removeFirst()
+            }}, completion: { [weak self] _ in
+                self?.button.isEnabled = true
+                })
         }
+    
+    // MARK: Random set methods
+    func getUniqueColors() -> Set<UIColor> {
+        var set = Set<UIColor>()
+        
+        while set.count < views.count {
+            set.insert(UIColor(
+                displayP3Red: .random(in: minColorComp...maxColorComp),
+                green: .random(in: minColorComp...maxColorComp),
+                blue: .random(in: minColorComp...maxColorComp),
+                alpha: colorAlpha
+            ))
+        }
+        
+        return set
     }
     
+    func getUniqueCorners() -> Set<Double> {
+        var set = Set<Double>()
+        while set.count < views.count {
+            set.insert(Double.random(in: minCornerRad...maxCornerRad))
+        }
+        return set
+    }
+
 
 }
 
